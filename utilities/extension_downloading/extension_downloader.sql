26f4bff557b44a83df43822dbc996bec105ecc4f
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-08-07 12:38:16 +0200

    Fixed the script

--- utilities/extension_downloading/extension_downloader.sql
+++ utilities/extension_downloading/extension_downloader.sql
@@ -8,15 +8,15 @@
 CREATE SCHEMA IF NOT EXISTS EXA_toolbox;
 
 --/
-CREATE OR REPLACE PYTHON3 SET SCRIPT EXA_toolbox.upload_to_bucket_with_link(script_holder_schema_name VARCHAR(1000), release_name VARCHAR(1000), git_hub_api_link VARCHAR(2000), upload_url VARCHAR(1000)) EMITS (outputs VARCHAR(2000000)) AS
+CREATE OR REPLACE PYTHON SET SCRIPT EXA_toolbox.upload_to_bucket_with_link(release_name VARCHAR(1000), git_hub_api_link VARCHAR(2000), upload_url VARCHAR(1000)) EMITS (outputs VARCHAR(2000000)) AS
 import requests
 
 def run(ctx):
-        r = requests.get('https://raw.githubusercontent.com/AnastasiiaSergienko/exa-toolbox/extension_downloader/utilities/extension_downloading/ContainerFileUploader.py', allow_redirects=True)
+        r = requests.get('https://raw.githubusercontent.com/exasol/exa-toolbox/master/utilities/extension_downloading/ContainerFileUploader.py', allow_redirects=True)
         with open('/tmp/ContainerFileUploader.py', 'wb') as f:
                 f.write(r.content)
 
-        r2 = requests.get('https://raw.githubusercontent.com/AnastasiiaSergienko/exa-toolbox/extension_downloader/utilities/extension_downloading/ReleaseLinkExtractor.py', allow_redirects=True)
+        r2 = requests.get('https://raw.githubusercontent.com/exasol/exa-toolbox/master/utilities/extension_downloading/ReleaseLinkExtractor.py', allow_redirects=True)
         with open('/tmp/ReleaseLinkExtractor.py', 'wb') as f2:
                 f2.write(r2.content)
 
@@ -24,7 +24,7 @@ def run(ctx):
         sys.path.append('/tmp')
 
         from ContainerFileUploader import ContainerFileUploader
-        file_uploader = ContainerFileUploader(ctx.script_holder_schema_name, ctx.release_name, ctx.git_hub_api_link)
+        file_uploader = ContainerFileUploader(ctx.release_name, ctx.git_hub_api_link)
         file_uploader.upload(ctx.upload_url)
 /
 
