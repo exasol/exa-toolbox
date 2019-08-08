84c00fe8f4569d36cf00e23e3de4aae8b0c0802d
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-08-08 16:16:19 +0200

    Fixed review findings

--- utilities/extension_downloading/ContainerFileUploader.py
+++ utilities/extension_downloading/ContainerFileUploader.py
@@ -21,10 +21,12 @@ class ContainerFileUploader:
         upload_url = self.__build_upload_url(address, username, password)
         requests.put(upload_url, data=r_download.iter_content(10 * 1024))
 
-    def __build_upload_url(self, address, username, password):
+    def __build_upload_url(self, address, username, password, path_inside_bucket):
         connection_first_part = 'http://'
         split_url = address.split(connection_first_part, 1)
-        return connection_first_part + username + ':' + password + '@' + split_url[1] + '/' + self.file_to_download_name
+        return "{connection_first_part}{username}:{password}@{url}/{path_inside_bucket}{file_to_download_name}".format(
+            connection_first_part=connection_first_part, username=username, password=password, url=split_url[1],
+            path_inside_bucket=path_inside_bucket, file_to_download_name=self.file_to_download_name)
 
     def __extract_download_url(self):
         github_api_link = self.__build_github_api_link()
@@ -33,4 +35,5 @@ class ContainerFileUploader:
         return download_url
 
     def __build_github_api_link(self):
-        return "https://api.github.com/repos/" + self.github_user + "/" + self.repository_name + "/releases/" + self.release_name
+        return "https://api.github.com/repos/{github_user}/{repository_name}/releases/{release_name}".format(
+            github_user=self.github_user, repository_name=self.repository_name, release_name=self.release_name)
--- utilities/extension_downloading/extension_downloader.sql
+++ utilities/extension_downloading/extension_downloader.sql
@@ -6,16 +6,8 @@
 
 CREATE SCHEMA IF NOT EXISTS EXA_toolbox;
 
-CREATE OR REPLACE CONNECTION BUCKET_CONNECTION
-      TO 'http://<host>:<port>/<bucket name>'
-      USER 'w'
-      IDENTIFIED BY '<writing password>';
-
--- Example:
--- CREATE OR REPLACE CONNECTION BUCKET_CONNECTION TO 'http://localhost:2580/test' USER 'w' IDENTIFIED BY 'password';
-
 --/
-CREATE OR REPLACE PYTHON SET SCRIPT EXA_toolbox.upload_to_bucket_with_link(connection_name VARCHAR(1000), file_to_download_name VARCHAR(1000), github_user VARCHAR(1000), repository_name VARCHAR(1000), release_name VARCHAR(1000))
+CREATE OR REPLACE PYTHON SET SCRIPT EXA_toolbox.upload_to_bucket_with_link(connection_name VARCHAR(1000), file_to_download_name VARCHAR(1000), github_user VARCHAR(1000), repository_name VARCHAR(1000), release_name VARCHAR(1000), path_inside_bucket VARCHAR(1000))
 EMITS (outputs VARCHAR(20000)) AS
 import requests
 import sys
@@ -32,7 +24,7 @@ def run(ctx):
         sys.path.append('/tmp')
 
         from ContainerFileUploader import ContainerFileUploader
-        file_uploader = ContainerFileUploader(ctx.file_to_download_name, ctx.github_user, ctx.repository_name, ctx.release_name)
+        file_uploader = ContainerFileUploader(ctx.file_to_download_name, ctx.github_user, ctx.repository_name, ctx.release_name, ctx.path_inside_bucket)
 
         bucket_connection_string = exa.get_connection(ctx.connection_name)
         file_uploader.upload(bucket_connection_string.address, bucket_connection_string.user, bucket_connection_string.password)
@@ -42,6 +34,9 @@ def run(ctx):
 -- SELECT upload_to_bucket_with_link('name of the connection', '<name of the file to upload from github>', '<name of the user holding the repository>', '<name of the repository>', '<name of the release>');
 
 -- Example:
+
+-- CREATE OR REPLACE CONNECTION BUCKET_CONNECTION TO 'http://<host>:<port>/<bucket name>' USER 'w' IDENTIFIED BY '<writing password>';
+
 -- SELECT upload_to_bucket_with_link('BUCKET_CONNECTION', 'python3-ds-EXASOL-6.1.0', 'exasol', 'script-languages', 'latest');
 
 -- EOF
