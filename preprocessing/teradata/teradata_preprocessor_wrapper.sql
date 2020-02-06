222476f8ee9db47e489dcf7a5ec878cd5722f9f6
Lennart Nieratschker <56670912+LennartAtExasol@users.noreply.github.com>
2020-02-06 14:20:16 +0100

    Add testcases, add infrastructure for index_to_locate

--- preprocessing/teradata/teradata_preprocessor_wrapper.sql
+++ preprocessing/teradata/teradata_preprocessor_wrapper.sql
@@ -3,11 +3,17 @@ alter session set SQL_PREPROCESSOR_SCRIPT = '';
  
 --/
 create or replace script preprocessing.TeradataPre() as
-import ('PREPROCESSING.TRANSFORMATIONS', 'TRANS')
-sqltext = sqlparsing.getsqltext()
-sqlparsing.setsqltext(TRANS.like_all_any(sqltext))
+ import ('PREPROCESSING.TRANSFORMATIONS', 'TRANS')
+ local sqltext = sqlparsing.getsqltext()
+ sqltext = TRANS.like_all_any(sqltext)
+ sqltext = TRANS.index_to_locate(sqltext)
+ 
+ sqlparsing.setsqltext(sqltext)
 /
 
 --SELECT * FROM retail.article WHERE description LIKE ALL('%in%',/*some comment*/'%Mix%','%c%') and description like all('%ini%', '%x%');
---SELECT * FROM retail.article WHERE description LIKE ALL('%in%',/*some comment*/'%Mix%','%c%') and description like any('%ini%', '%x%');
+--SELECT * FROM retail.article WHERE description LIKE any('%in%',/*some comment*/'%Mix%','%c%') and description like all('%ini%', '%x%');
 --select * from retail.article where description like all('%in%', '%ins%') and (description like any('%in%', '%in%') or description like all ('%ba%','%ce%'));
+--select locate('oma', description) as col1 from retail.article where locate('oma', description) > 0;
+--select index(description, 'oma') as col1 from retail.article where index(description, 'oma') > 0;
+--select *  from retail.article where description like any('%be%','%s%');
